3858668e4931839b8863116153ae85a45a6b2191
giuseppezaccato <giuseppezaccato@gmail.com>
2025-01-18 22:28:39 +0100

    END: lastfix

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 //task trovo e dichiaro elementi
 const lamp = document.getElementById('white_lamp');
 const button = document.getElementById('btn');
+const power = document.getElementById("power")
 
 //task eventlistener al button con arrow/ternary IF
-button.addEventListener('click', () =>
-(button.innerHTML.includes("ACCENDI")
+button.addEventListener('click', () => {
+    button.innerHTML.includes("ACCENDI")
     ? (lamp.src = "./img/yellow_lamp.png", button.innerHTML = "SPEGNI")
     : (lamp.src = "./img/white_lamp.png", button.innerHTML = "ACCENDI")
-));
+}
+);
 
-
-
-
-//---BONUS---piccolo bonus lavorando con le classi e modificando quelle di bootstrap...
+//---piccolo bonus lavorando con le classi e modificando quelle di bootstrap...non funziona, compare un altro tasto a fianco -.-""
 //task riconosco l'elemento
 // const button1 = document.getElementById('btn1');
 
@@ -20,8 +19,8 @@ button.addEventListener('click', () =>
 //* voglio che il btn da btn-outline-dark cambi classe e diventi btn-outline-waning
 // button1.addEventListener('click', () =>
 // (button1.innerHTML.includes("ON")
-//     ? (lamp.src = "./img/yellow_lamp.png", button.innerHTML = "OFF")
-//     : (lamp.src = "./img/white_lamp.png", button.innerHTML = "ON")
+//     ? (button1.classList.replace('btn-outline-dark', 'btn-outline-waning'), lamp.src = "./img/yellow_lamp.png", button.innerHTML = "OFF")
+//     : (button1.classList.replace('btn-outline-warning', 'btn-outline-dark'), lamp.src = "./img/white_lamp.png", button.innerHTML = "ON")
 // ));
 
 
